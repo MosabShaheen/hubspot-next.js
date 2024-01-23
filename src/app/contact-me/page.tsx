8667995716bbfa72d9468817fd3831fcab3295cf
MosabShaheen <99540632+MosabShaheen@users.noreply.github.com>
2024-01-23 17:41:51 +0500

    error resolved

--- src/app/contact-me/page.tsx
+++
@@ -1,9 +0,0 @@
-import Contact from "../contact/page";
-
-export default function ContactMe(){
-    return(
-        <div>
-            <Contact />
-        </div>
-    )
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,7 @@ import Link from "next/link";
 export default function Home() {
     return(
         <div>
-            <Link href="/contact" >Direct hubspot integration</Link>
-            <Link href="/contact-me" >Through API hubspot integration</Link>
+            <Link className="px-10 py-10 bg-red-700" href="/contact" >Direct hubspot integration</Link>
         </div>
     )
 }
